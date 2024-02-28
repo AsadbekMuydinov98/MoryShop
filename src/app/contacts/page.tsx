1167fa082b16a615c4bebfd5a59695a3b7dc4cbc
Asadbek <you@example.com>
2024-02-28 16:04:15 +0900

    added4

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -9,7 +9,7 @@ const ContactsPage = () => {
 						className='absolute inset-0'
 						frameBorder='0'
 						title='map'
-						marginheight='0'
+						marginHeight='0'
 						marginwidth='0'
 						scrolling='no'
 						src='https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed'
